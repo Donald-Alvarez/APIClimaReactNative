ea94aaca2430b4fe49b631289fef1e83eaa7d38c
Donald-Alvarez <donaldoctavio0210@gmail.com>
2023-06-29 23:17:40 -0600

    Creacion de consumo y validacion del API

--- src/ClimaScreen.js
+++ src/ClimaScreen.js
@@ -7,5 +7,20 @@ const ClimaScreen = () => {
     const [climaData, setClimaData] = useState(null);
     // Lista de países que deseo mostrar
     const countries = ['Nicaragua', 'Honduras', 'Costa Rica', 'El Salvador', 'Panama',];
-  
+    useEffect(() => {
+        const fetchClimaData = async () => {
+          try {
+            const responsePromises = countries.map(country =>
+              axios.get(`https://api.weatherapi.com/v1/current.json?key=105307eb1bd949c699a235621232806&q=${country}&lang=es`)
+            );
+            const responses = await Promise.all(responsePromises);
+            const climaDataArray = responses.map(response => response.data);
+            setClimaData(climaDataArray);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+    
+        fetchClimaData();
+      }, []); 
 }
